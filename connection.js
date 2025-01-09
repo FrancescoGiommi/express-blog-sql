7eb8545fbbd7ce6093bce14b67ccb89dbe31722a
FrancescoGiommi <francescogiommi97@icloud.com>
2025-01-09 15:24:01 +0100

    Add fix

--- connection.js
+++ connection.js
@@ -2,8 +2,8 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
-  database: "pizzeria",
+  password: "password",
+  database: "blog",
 });
 connection.connect((err) => {
   if (err) throw err;
